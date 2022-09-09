14b86a1d72ef32aa47d1525c82a8221d85fc0bea
Tim Empey <timothyempey@gmail.com>
2022-09-09 15:00:14 -0700

    add enriched data API

--- src/converter.js
+++ src/converter.js
@@ -14,3 +14,20 @@ export default class CurrencyConverter {
       });
   }
 }
+
+export class CurrencyData {
+  static getData(newCurrency) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/enriched/USD/${newCurrency}`)
+      .then(function(response) {
+        if (!response.ok) {
+          const errorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
+        } else {
+          return response.json();
+        }
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
+}
