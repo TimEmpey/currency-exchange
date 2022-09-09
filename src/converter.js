6c9116b045aaa8484879546dbb453909bebbc215
Tim Empey <timothyempey@gmail.com>
2022-09-09 15:33:36 -0700

    change base/ target currency to a text input

--- src/converter.js
+++ src/converter.js
@@ -1,23 +1,6 @@
 export default class CurrencyConverter {
-  static getCurrency(newCurrency, usdAmount) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${newCurrency}/${usdAmount}`)
-      .then(function(response) {
-        if (!response.ok) {
-          const errorMessage = `${response.status} ${response.statusText}`;
-          throw new Error(errorMessage);
-        } else {
-          return response.json();
-        }
-      })
-      .catch(function(error) {
-        return error;
-      });
-  }
-}
-
-export class CurrencyData {
-  static getData(newCurrency) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/enriched/USD/${newCurrency}`)
+  static getCurrency(baseCurrency, newCurrency, usdAmount) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${baseCurrency}/${newCurrency}/${usdAmount}`)
       .then(function(response) {
         if (!response.ok) {
           const errorMessage = `${response.status} ${response.statusText}`;
--- src/index.js
+++ src/index.js
@@ -3,37 +3,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyConverter from './converter.js';
 
-function getCurrency(newCurrency, usdAmount) {
-  CurrencyConverter.getCurrency(newCurrency, usdAmount)
+function getCurrency(baseCurrency, newCurrency, usdAmount) {
+  CurrencyConverter.getCurrency(baseCurrency, newCurrency, usdAmount)
     .then(function(response) {
       if(response.conversion_result) {
-        printElements(response, newCurrency, usdAmount);
+        printElements(response, baseCurrency, newCurrency, usdAmount);
       }  else {
-        printError(response, newCurrency, usdAmount);
+        printError(response, baseCurrency,  newCurrency, usdAmount);
       }
     });
 }
 
-function printElements (response, newCurrency, usdAmount) {
+function printElements (response, baseCurrency, newCurrency, usdAmount) {
   let conversionAmount = response.conversion_result;
-  document.getElementById("conversion-output").innerText = `Conversion: ${usdAmount} USD is worth ${conversionAmount} ${newCurrency}`;
+  document.getElementById("conversion-output").innerText = `Conversion: ${usdAmount} ${baseCurrency} is worth ${conversionAmount} ${newCurrency}`;
 }
 
-function printError (error, newCurrency) {
+function printError (error, baseCurrency, newCurrency) {
   let outputs = document.getElementById("conversion-output");
   outputs.innerHTML = null;
-  outputs.innerText = `There was an error accessing information for ${newCurrency}\n ${error}`;
+  outputs.innerText = `There was an error accessing information for ${baseCurrency} or ${newCurrency}\n ${error}`;
 }
 
 function handleForm (e) {
   e.preventDefault();
   let outputs = document.getElementById("conversion-output");
   outputs.innerHTML = null;
+  let baseCurrency = document.getElementById("base-currency").value;
   let newCurrency = document.getElementById("target-currency").value;
   let usdAmount = parseInt(document.querySelector("input#usd-input").value);
-  document.getElementById("target-currency").value = null;
-  document.querySelector("input#usd-input").value = null;
-  getCurrency(newCurrency, usdAmount);
+  getCurrency(baseCurrency, newCurrency, usdAmount);
 }
 
 window.addEventListener("load", function() {
