17a60a6005897cb7a1869fbb8c9e58f2b888c03b
Tim Empey <timothyempey@gmail.com>
2022-09-09 12:55:24 -0700

    fix API 404 bug

--- src/converter.js
+++ src/converter.js
@@ -1,6 +1,6 @@
 export default class CurrencyConverter {
   static getCurrency(newCurrency, usdAmount) {
-    return fetch(`https://v6.exchangerate-api.com/v6/bb4ee8704dc0b83ec038e1f4/pair/USD/${newCurrency}/${usdAmount}}`)
+    return fetch(`https://v6.exchangerate-api.com/v6/bb4ee8704dc0b83ec038e1f4/pair/USD/${newCurrency}/${usdAmount}`)
       .then(function(response) {
         if (!response.ok) {
           const errorMessage = `${response.status} ${response.statusText}`;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import CurrencyConverter from './converter';
+import CurrencyConverter from './converter.js';
 
 function getCurrency(newCurrency, usdAmount) {
-  CurrencyConverter.getCurrency(newCurrency)
+  CurrencyConverter.getCurrency(newCurrency, usdAmount)
     .then(function(response) {
-      if(response.conversion_results) {
+      if(response.conversion_result) {
         printElements(response, newCurrency, usdAmount);
       }  else {
         printError(response, newCurrency, usdAmount);
@@ -15,14 +15,14 @@ function getCurrency(newCurrency, usdAmount) {
 }
 
 function printElements (response, newCurrency, usdAmount) {
-  let conversionAmount = response.conversion_results;
+  let conversionAmount = response.conversion_result;
   document.getElementById("conversion-output").innerText = `Conversion: ${usdAmount} USD is worth ${conversionAmount} ${newCurrency}`;
 }
 
 function printError (error, newCurrency) {
   let outputs = document.getElementById("conversion-output");
   outputs.innerHTML = null;
-  outputs.innerText = `There was an error accessing information for ${newCurrency}: ${error}`;
+  outputs.innerText = `There was an error accessing information for ${newCurrency}\n ${error}`;
 }
 
 function handleForm (e) {
@@ -30,10 +30,7 @@ function handleForm (e) {
   let outputs = document.getElementById("conversion-output");
   outputs.innerHTML = null;
   let newCurrency = document.getElementById("target-currency").value;
-  console.log(newCurrency);
   let usdAmount = parseInt(document.querySelector("input#usd-input").value);
-  console.log(usdAmount);
-  console.log(typeof usdAmount);
   document.getElementById("target-currency").value = null;
   document.querySelector("input#usd-input").value = null;
   getCurrency(newCurrency, usdAmount);
