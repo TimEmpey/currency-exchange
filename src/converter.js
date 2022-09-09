f31300fb4bb9e3fe148fb2f5c1b8cb4c98ec9864
Tim Empey <timothyempey@gmail.com>
2022-09-09 12:11:33 -0700

    add printError and printElement functionality

--- src/converter.js
+++ src/converter.js
@@ -1,6 +1,6 @@
 export default class CurrencyConverter {
-  static getCurrency(newCurrency) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${newCurrency}}`)
+  static getCurrency(newCurrency, usdAmount) {
+    return fetch(`https://v6.exchangerate-api.com/v6/bb4ee8704dc0b83ec038e1f4/pair/USD/${newCurrency}/${usdAmount}}`)
       .then(function(response) {
         if (!response.ok) {
           const errorMessage = `${response.status} ${response.statusText}`;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyConverter from './converter';
+
+function getCurrency(newCurrency, usdAmount) {
+  CurrencyConverter.getCurrency(newCurrency)
+    .then(function(response) {
+      if(response.conversion_results) {
+        printElements(response, newCurrency, usdAmount);
+      }  else {
+        printError(response, newCurrency, usdAmount);
+      }
+    });
+}
+
+function printElements (response, newCurrency, usdAmount) {
+  let conversionAmount = response.conversion_results;
+  document.getElementById("conversion-output").innerText = `Conversion: ${usdAmount} USD is worth ${conversionAmount} ${newCurrency}`;
+}
+
+function printError (error, newCurrency) {
+  let outputs = document.getElementById("conversion-output");
+  outputs.innerHTML = null;
+  outputs.innerText = `There was an error accessing information for ${newCurrency}: ${error}`;
+}
+
+function handleForm (e) {
+  e.preventDefault();
+  let outputs = document.getElementById("conversion-output");
+  outputs.innerHTML = null;
+  let newCurrency = document.getElementById("target-currency").value;
+  console.log(newCurrency);
+  let usdAmount = parseInt(document.querySelector("input#usd-input").value);
+  console.log(usdAmount);
+  console.log(typeof usdAmount);
+  document.getElementById("target-currency").value = null;
+  document.querySelector("input#usd-input").value = null;
+  getCurrency(newCurrency, usdAmount);
+}
+
+window.addEventListener("load", function() {
+  document.querySelector('form').addEventListener("submit", handleForm);
+});
